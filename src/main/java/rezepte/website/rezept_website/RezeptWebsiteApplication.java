51180be4da4eb578789feb44e1f1fd3c639d6284
Kxrstin <147599239+Kxrstin@users.noreply.github.com>
2025-08-10 15:12:12 +0200

    Add files via upload

---
+++ src/main/java/rezepte/website/rezept_website/RezeptWebsiteApplication.java
@@ -0,0 +1,13 @@
+package rezepte.website.rezept_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RezeptWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RezeptWebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rezepte/website/rezept_website/RezeptWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package rezepte.website.rezept_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RezeptWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
