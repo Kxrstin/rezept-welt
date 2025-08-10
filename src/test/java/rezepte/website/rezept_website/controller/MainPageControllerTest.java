7d4e67d70e12030b96c757bec2c68132a228af35
Kxrstin <kerstinneu.damme@gmail.com>
2025-08-10 21:35:10 +0200

    FIX Tests
    Mockito Bean Service injected, damit die Tests weiterhin laufen.

--- src/test/java/rezepte/website/rezept_website/controller/MainPageControllerTest.java
+++ src/test/java/rezepte/website/rezept_website/controller/MainPageControllerTest.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import rezepte.website.rezept_website.service.RezeptService;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -15,6 +17,9 @@ public class MainPageControllerTest {
 
     @Autowired
     MockMvc mvc;
+
+    @MockitoBean
+    RezeptService service;
 
     @Test
     @DisplayName("Die Startseite ist unter / erreichbar.")
@@ -30,5 +35,13 @@ public class MainPageControllerTest {
         mvc.perform(get("/add/rezept"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("add_rezept"));
+    }
+
+    @Test
+    @DisplayName("Beim erfolgreichen Hinzufügen eines Rezeptes erscheint eine Success Message auf der Main Page")
+    void test_success() throws Exception{
+        mvc.perform(get("/?success=true"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("main_page"));
     }
 }
