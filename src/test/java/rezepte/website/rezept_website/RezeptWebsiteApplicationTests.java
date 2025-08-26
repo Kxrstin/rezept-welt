3cc31e6779b0eccaadd8468789f2e9c6ef9ec887
Kxrstin <kerstinneu.damme@gmail.com>
2025-08-26 13:13:12 +0200

    FIX Application Test mit Testcontainer

--- src/test/java/rezepte/website/rezept_website/RezeptWebsiteApplicationTests.java
+++ src/test/java/rezepte/website/rezept_website/RezeptWebsiteApplicationTests.java
@@ -2,12 +2,31 @@ package rezepte.website.rezept_website;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 @SpringBootTest
 class RezeptWebsiteApplicationTests {
 
+    // PostgreSQL Container starten (mit Docker)
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16")
+            .withDatabaseName("testdb")
+            .withUsername("test")
+            .withPassword("test");
+
+    static {
+        postgres.start();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
     @Test
     void contextLoads() {
     }
-
 }
